7e5a4b58e85bddc73a4f99d1e73881a373bbb452
Jay-R-Mac <makee2992@gmail.com>
2023-10-24 16:00:20 +0100

    added boxes to seperate comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -16,7 +16,7 @@ export default function Comments(props) {
 <div className="bg-gray-100 p-4 rounded-lg shadow-lg">
   <h3 className="text-2xl font-bold mb-4">Comments Section</h3>
   {comments.map((comment) => (
-    <div className="mb-4" key={comment.id}>
+    <div className="mb-4 border border-gray-300 p-4 rounded" key={comment.id}>
       <p className="text-red-500 font-semibold mb-1">{comment.author}</p>
       <p className="text-gray-700 mb-1">Votes: {comment.votes}</p>
       <p className="text-gray-800 mb-1">{comment.body}</p>
