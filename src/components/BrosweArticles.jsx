b771262cfa4946870f3b73a4b09abe03fbd76e90
Jay-R-Mac <makee2992@gmail.com>
2023-10-24 13:42:42 +0100

    added individual articles and some styling

--- src/components/BrosweArticles.jsx
+++ src/components/BrosweArticles.jsx
@@ -18,13 +18,16 @@ export default function BrowseArticles() {
       {articles.map((article) => {
         return (
           <div className="flex-wrap row-span-2 container mx-auto py-2 box-border max-w-2xl box-decoration-slice bg-stone-200 border-4  border-red-500 shadow-2xl">
-            <p className="text-xl text-center underline">{article.title}</p>{" "}
+            <a className="text-xl text-center underline" href={`http://localhost:5173/article/${article.article_id}`}>{article.title}</a>{" "}
             <div className="">
               {" "}
+             <a href={`http://localhost:5173/article/${article.article_id}`}>
               <img
                 className="mx-auto text-center object-contain h-48 w-96"
+                
                 src={article.article_img_url} alt="Article Image"
-              />
+                
+              /></a>
             </div>
             <p className="text-center text-green-900">
               {" "}
---
+++ src/components/IndividualArticle.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { getArticleById } from "./Axios";
+import { useParams } from "react-router-dom";
+
+export default function IndividualArticle() {
+  const { article_id } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [article, setArticleInfo] = useState([]);
+
+  useEffect(() => {
+    getArticleById(article_id).then((response) => {
+      console.log(response);
+      setArticleInfo(response);
+      setIsLoading(false);
+    });
+  }, []);
+
+  if (isLoading) return <p>Loading Please Wait...</p>;
+
+  return (
+<div className="max-w-4xl mx-auto my-8 p-4 bg-white border rounded-lg shadow-lg">
+  <h2 className="text-2xl font-bold mb-2">{article.title}</h2>
+  <p className="text-gray-600 mb-4">Author: {article.author}</p>
+
+  <div className="flex justify-center">
+    <img
+      className="object-contain w-full h-full"
+      src={article.article_img_url}
+      alt="Article Image"
+    />
+  </div>
+
+  <div className="mt-4 leading-7">{article.body}</div>
+</div>
+
+
+
+  );
+}
