c0d6870099574c780f8627a0ba62bb642694a423
Jay-R-Mac <makee2992@gmail.com>
2023-10-27 15:33:01 +0100

    added a delete comment function, only available on the users own comments while logged in, removed article votes as voter component is not yet implemented for comments

--- src/components/CommentDeleter.jsx
+++ src/components/CommentDeleter.jsx
@@ -1,21 +1,36 @@
-// import { deleteArticleComments } from "./Axios";
+import { deleteArticleComments } from "./Axios";
 import { useEffect, useState, useContext } from "react";
+import { UserContext } from "../App";
 
-
-
-export default function CommentDeleter({comment_id, commentAuthor}) {
+export default function CommentDeleter({ comment_id, commentAuthor }) {
   const [deleteComment, setDeleteComment] = useState(false);
+  const [signedIn] =useContext(UserContext)
 
 
-//   useEffect(() => {  
-//     deleteArticleComments(comment_id).then((response) => {
-//       setComments(response);
-//     });
-//   }, [comments]);
+  const handleDeleteComment = () => {
+    deleteArticleComments(comment_id).then((response) => {
+      console.log(response);
+    });
+  };
 
   return (
-    <div>
-        Delete me
+    <div className="mt-4">
+      {signedIn === commentAuthor && (
+        <form
+          className="flex items-center mt-4"
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleDeleteComment();
+          }}
+        >
+          <button
+            type="submit"
+            className="bg-red-700 hover:bg-red-900 text-white font-bold py-1 px-1 rounded"
+          >
+            Delete
+          </button>
+        </form>
+      )}
     </div>
   );
 }
