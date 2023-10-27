0fbad3a866528321393ff876a4b0e032defe88f4
Jay-R-Mac <makee2992@gmail.com>
2023-10-27 16:09:53 +0100

    disabled delete button on click, added an indicator for loading on deletion

--- src/components/CommentDeleter.jsx
+++ src/components/CommentDeleter.jsx
@@ -3,13 +3,15 @@ import { useEffect, useState, useContext } from "react";
 import { UserContext } from "../App";
 
 export default function CommentDeleter({ comment_id, commentAuthor }) {
-  const [deleteComment, setDeleteComment] = useState(false);
-  const [signedIn] =useContext(UserContext)
-
+  const [signedIn] = useContext(UserContext);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleDeleteComment = () => {
-    deleteArticleComments(comment_id).then((response) => {
-      console.log(response);
+    setIsDisabled(true);
+    setIsLoading(true)
+    deleteArticleComments(comment_id).then((res) => {
+      if (res.status === 204){setIsDisabled(false), setIsLoading(false)}
     });
   };
 
@@ -19,11 +21,13 @@ export default function CommentDeleter({ comment_id, commentAuthor }) {
         <form
           className="flex items-center mt-4"
           onSubmit={(e) => {
-            e.preventDefault();
+            e.preventDefault()
             handleDeleteComment();
           }}
         >
+          {signedIn && (<p hidden={!isLoading} className="text-red-700">Deleting...</p>)}
           <button
+            disabled={isDisabled}
             type="submit"
             className="bg-red-700 hover:bg-red-900 text-white font-bold py-1 px-1 rounded"
           >
