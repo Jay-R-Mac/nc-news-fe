812ab8c1b519ea222c27d62360d939d84016d5fd
Jay-R-Mac <makee2992@gmail.com>
2023-10-29 14:54:16 +0000

    added 404 error handling and added instructions to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,9 @@
-export default function Home(){
-    return <p className="text-md text-center my-4">Welcome to the homepage</p>;
-
+export default function HomePage() {
+    return (
+        <div className="text-center">
+            <h2 className="text-2xl font-bold my-4">Welcome to the homepage</h2>
+            <p className="mb-4">To begin using this site, please use the navigation bar at the top to move around the site.</p> <p className="mb-4">Some features are only available to signed-in profiles.</p>
+            <p className="mb-4"> You can sign in on the users page to gain access to these features.</p>
+        </div>
+    );
 }
--- src/components/IndividualArticle.jsx
+++ src/components/IndividualArticle.jsx
@@ -2,21 +2,26 @@ import { useEffect, useState } from "react";
 import { getArticleById } from "./Axios";
 import { useParams } from "react-router-dom";
 import Comments from "./Comments";
+import ErrorPage from "./ErrorPage";
 
 export default function IndividualArticle() {
   const { article_id } = useParams();
   const [isLoading, setIsLoading] = useState(true);
-
+  const [errorStatus, setErrorStatus] = useState(null)
   const [article, setArticleInfo] = useState([]);
-
+console.log(article, errorStatus)
   useEffect(() => {
     getArticleById(article_id).then((response) => {
       setArticleInfo(response);
       setIsLoading(false);
-    });
-  }, []);
-
+    })
+    .catch((error)=>{setErrorStatus(error.response.status), setIsLoading(false)}
+    );
+  }, [errorStatus]);
+  
   if (isLoading) return <p>Loading Please Wait...</p>;
+  if (errorStatus === 404) {return (<ErrorPage />)}
+
 
   return (
     <div className="max-w-2xl mx-auto my-8 p-4 bg-white border rounded-lg shadow-lg">
